5ccb62a5899cfaae997043402971dd6b6baee178
Ratnakar <kezy485@gmail.com>
2025-01-06 16:39:39 +0530

    Add required variable for quantities

--- lib/shopify_graphql_helper/queries/find_product_variant_query.rb
+++ lib/shopify_graphql_helper/queries/find_product_variant_query.rb
@@ -21,7 +21,7 @@ module ShopifyGraphqlHelper
                   location{
                     id
                   }
-                  quantities {
+                  quantities(names: "available"){
                     name
                     quantity
                   }

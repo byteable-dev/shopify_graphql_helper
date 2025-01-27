c7c78956f772c95f573839b2c1b719c090ab406d
Ratnakar <kezy485@gmail.com>
2025-01-27 14:17:05 +0530

    Fix unintialized constang

--- lib/shopify_graphql_helper/query_resolver.rb
+++ lib/shopify_graphql_helper/query_resolver.rb
@@ -12,7 +12,7 @@ module ShopifyGraphqlHelper
       find_product_variant: ShopifyGraphqlHelper::Queries::FindProductVariantQuery,
       find_order: ShopifyGraphqlHelper::Queries::FindOrderQuery,
       find_location: ShopifyGraphqlHelper::Queries::FindLocationQuery,
-      find_product_by_search_query: FindProductBySearchQuery,
+      find_product_by_search_query: ShopifyGraphqlHelper::Queries::FindProductBySearchQuery,
       staff_members: ShopifyGraphqlHelper::Queries::StaffMembersQuery,
       orders: ShopifyGraphqlHelper::Queries::OrdersQuery,
       locations: ShopifyGraphqlHelper::Queries::LocationsQuery

ffe943f7885963f876e9b92118c0b3293a843d1c
Ratnakar <kezy485@gmail.com>
2025-01-29 15:22:46 +0530

    Search orders query updated

--- lib/shopify_graphql_helper/queries/search_orders_query.rb
+++ lib/shopify_graphql_helper/queries/search_orders_query.rb
@@ -10,25 +10,135 @@ module ShopifyGraphqlHelper
               id
               legacyResourceId
               name
+              email
+              note
+              currencyCode
               displayFulfillmentStatus
               displayFinancialStatus
               createdAt
+              totalPriceSet{
+                presentmentMoney {
+                  amount
+                  currencyCode
+                }
+                shopMoney{
+                  amount
+                  currencyCode
+                }
+              }
+              totalTaxSet{
+                presentmentMoney {
+                  amount
+                  currencyCode
+                }
+                shopMoney{
+                  amount
+                  currencyCode
+                }
+              }
+              totalShippingPriceSet{
+                presentmentMoney {
+                  amount
+                  currencyCode
+                }
+                shopMoney{
+                  amount
+                  currencyCode
+                }
+              }
+              billingAddress{
+                address1
+                address2
+                city
+                company
+                country
+                countryCodeV2
+                firstName
+                lastName
+                name
+                phone
+                timeZone
+                zip
+                countryCode
+              }
+              app {
+                id
+              }
               customer {
                 id
                 firstName
+                lastName
                 displayName
+                email
+                defaultAddress{
+                  address1
+                  address2
+                  city
+                  company
+                  zip
+                  countryCode
+                }
+                emailMarketingConsent{
+                  consentUpdatedAt
+                  marketingState
+                }
+                smsMarketingConsent{
+                  consentUpdatedAt
+                  marketingState
+                }
+              }
+              staffMember{
+                id
+                email
               }
               lineItems(first: 250) {
                 nodes {
                   id
                   name
+                  title
                   sku
                   quantity
+                  image{
+                    url
+                  }
+                  originalUnitPriceSet{
+                    presentmentMoney {
+                      amount
+                      currencyCode
+                    }
+                    shopMoney{
+                      amount
+                      currencyCode
+                    }
+                  }
+                  originalTotalSet{
+                    presentmentMoney {
+                      amount
+                      currencyCode
+                    }
+                    shopMoney{
+                      amount
+                      currencyCode
+                    }
+                  }
+                  totalDiscountSet {
+                    presentmentMoney {
+                      amount
+                      currencyCode
+                    }
+                    shopMoney{
+                      amount
+                      currencyCode
+                    }
+                  }
                   variant {
                     id
+                    legacyResourceId
+                    title
                   }
                   product {
                     id
+                    legacyResourceId
                   }
                   customAttributes{
                     key

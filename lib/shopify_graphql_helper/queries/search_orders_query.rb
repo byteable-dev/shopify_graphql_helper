eaf2c6313861a34f2fdcb06de309219ce81a6f85
Ratnakar <kezy485@gmail.com>
2025-01-30 14:39:34 +0530

    Add phone to search orders query

--- lib/shopify_graphql_helper/queries/search_orders_query.rb
+++ lib/shopify_graphql_helper/queries/search_orders_query.rb
@@ -11,6 +11,7 @@ module ShopifyGraphqlHelper
               legacyResourceId
               name
               email
+              phone
               note
               currencyCode
               displayFulfillmentStatus

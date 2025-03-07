b88bacb3d904e9612e3c93e72df2aa3f8e6b40c7
Ratnakar <kezy485@gmail.com>
2025-03-07 15:53:53 +0530

    Add SearchMetafieldDefinition Query

---
+++ lib/shopify_graphql_helper/queries/search_metafield_definition_query.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module ShopifyGraphqlHelper
+  module Queries
+    class SearchMetafieldDefinitionQuery
+      QUERY = <<~QUERY
+        query($query: String!, $ownerType: MetafieldOwnerType) {
+          metafieldDefinitions(first: 50, ownerType: $ownerType, query: $query) {
+            nodes {
+              id
+              name
+              description
+              key
+              namespace
+              type{
+                name
+                category
+              }
+              ownerType
+              metafields(first: 20){
+                nodes{
+                  id
+                  legacyResourceId
+                  key
+                  value
+                  type
+                  namespace
+                  ownerType
+                }
+              }
+            }
+          }
+        }
+      QUERY
+
+      def self.query
+        QUERY
+      end
+    end
+  end
+end
--- lib/shopify_graphql_helper/query_resolver.rb
+++ lib/shopify_graphql_helper/query_resolver.rb
@@ -22,7 +22,8 @@ module ShopifyGraphqlHelper
       fulfillment_orders: ShopifyGraphqlHelper::Queries::FulfillmentOrdersQuery,
       locations: ShopifyGraphqlHelper::Queries::LocationsQuery,
       inventory_items: ShopifyGraphqlHelper::Queries::InventoryItemsQuery,
-      search_product_variant_metfeild_definitions: ShopifyGraphqlHelper::Queries::SearchProductVariantMetafieldDefiniations
+      search_product_variant_metfeild_definitions: ShopifyGraphqlHelper::Queries::SearchProductVariantMetafieldDefiniations,
+      search_metafield_definition: ShopifyGraphqlHelper::Queries::SearchMetafieldDefinitionQuery
     }.freeze
 
     MUTATIONS = {

a89fafb28809d21bb988820c69f9bd728dc7e262
Ratnakar <kezy485@gmail.com>
2025-03-20 12:29:17 +0530

    Update product variant

--- lib/shopify_graphql_helper/queries/find_product_variant_query.rb
+++ lib/shopify_graphql_helper/queries/find_product_variant_query.rb
@@ -7,32 +7,110 @@ module ShopifyGraphqlHelper
         query GetProductVariant($id: ID!) {
           productVariant(id: $id) {
             id
+            legacyResourceId
+            inventoryQuantity
+            inventoryPolicy
             title
             displayName
             sku
             barcode
             price
             compareAtPrice
-            availableForSale
-            createdAt
-            product {
+            taxable
+            selectedOptions{
+              name
+              value
+              optionValue{
+                id
+                name
+              }
+            }
+            media(first: 10){
+              nodes{
+                id
+                alt
+                mediaContentType
+                preview {
+                  image {
+                    id
+                    url
+                  }
+                }
+              }
+            }
+            metafields(first: 20){
+              nodes{
+                id
+                legacyResourceId
+                key
+                value
+                type
+                namespace
+                ownerType
+                description
+                jsonValue
+                definition{
+                  name
+                  type{
+                    category
+                    name
+                    supportedValidations{
+                      name
+                      type
+                    }
+                  }
+                  validations{
+                    name
+                    type
+                    value
+                  }
+                }
+              }
+            }
+            product{
               id
+              title
+              descriptionHtml
+              options(first: 20) {
+                id
+                position
+                name
+                values
+                optionValues {
+                  id
+                  name
+                  hasVariants
+                }
+              }
             }
             inventoryItem{
               id
               legacyResourceId
               sku
+              tracked
+              countryCodeOfOrigin
+              duplicateSkuCount
+              createdAt
+              provinceCodeOfOrigin
+              requiresShipping
+              measurement{
+                id
+                weight{
+                  unit
+                  value
+                }
+              }
               unitCost{
                 amount
                 currencyCode
               }
               inventoryLevels(first: 100) {
                 nodes {
+                  id
                   location{
                     id
-                    legacyResourceId
                   }
-                  quantities(names: "available"){
+                  quantities(names: "available") {
                     name
                     quantity
                   }

2baaddb11830aa4db247cfad43a1d56c1dadb8dd
Ratnakar <kezy485@gmail.com>
2025-04-02 20:05:55 +0530

    Add Id

--- lib/shopify_graphql_helper/queries/find_collection_by_title_query.rb
+++ lib/shopify_graphql_helper/queries/find_collection_by_title_query.rb
@@ -24,6 +24,7 @@ module ShopifyGraphqlHelper
                   publication{
                     autoPublish
                     name
+                    id
                   }
                 }
               }
